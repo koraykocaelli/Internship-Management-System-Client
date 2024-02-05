5289994c9482dff7772b576ad7edcce0f7805f80
Koray Kocaelli <koraykocaelli@outlook.com>
2024-02-05 18:52:26 +0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { AdvisorPortalModule } from './advisor-portal/advisor-portal.module';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AdminComponent } from './services/admin/admin.component';
+import { UiComponent } from './services/ui/ui.component';
+import { AdvisorPortalComponent } from './services/advisor-portal/advisor-portal.component';
+import { StudentPortalComponent } from './services/student-portal/student-portal.component';
 
 
 
@@ -19,6 +23,10 @@ import { NavbarComponent } from './navbar/navbar.component';
   declarations: [
     AppComponent,
     NavbarComponent,
+    AdminComponent,
+    UiComponent,
+    AdvisorPortalComponent,
+    StudentPortalComponent,
 
   ],
   imports: [
@@ -31,7 +39,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     NgxSpinnerModule,
     HttpClientModule,    
   ],
-  providers: [],
+  providers: [
+    {provide: "baseUrl", useValue: "" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
