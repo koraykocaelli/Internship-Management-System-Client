3378786c20be358669b446bb3f76eced0453df10
Koray Kocaelli <koraykocaelli@outlook.com>
2024-02-05 18:53:28 +0300

    aasd

---
+++ src/app/services/ui/ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrl: './ui.component.scss'
+})
+export class UiComponent {
+
+}
