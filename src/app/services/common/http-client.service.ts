61a30f2208b06f0d18949fba92e64b19c2404264
Koray Kocaelli <koraykocaelli@outlook.com>
2024-02-05 18:53:28 +0300

    aasd

---
+++ src/app/services/common/http-client.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  get<T>(){
+
+  }
+
+  post(){
+
+  }
+
+  put(){
+
+  }
+
+  delete(){
+
+  }
+
+
+}
+
+
---
+++ src/app/services/student-portal/student-portal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-portal',
+  templateUrl: './student-portal.component.html',
+  styleUrl: './student-portal.component.scss'
+})
+export class StudentPortalComponent {
+
+}
